ca61e32a217cad622f5075cff75a740a5195827e
Charles Dorval (deskwizard) <deskwizard@gmail.com>
2023-10-04 10:49:38 -0400

    Moved pins around for easier wiring

--- src/pins.h
+++ src/pins.h
@@ -1,44 +1,45 @@
 #ifndef _PINS_H_
 #define _PINS_H_
 
-// Keep the noisy pins on the opposite side
-// from the DAC and ADC as much as possible
-
 /*
     Currently Free:
-        D2
-        D6
         12 (MISO, not used for display)
+        A2
+        A3
         A6 Analog only (fan tach charge cap something?)
 */
 
 // TX   0
 // RX   1
-#define pinFanTach 3
+#define pinFanTach 2
+#define pinFanPWM 3
 #define pinFanEnable 4
-#define pinFanPWM 5 // Timer 0
-#define pinRangeRelay 6
-#define pinEnableRelay 7
-#define pinRangeSwitch 8
 
-// SPI SS       10      (Yellow)
-// SPI MOSI     11      (Purple)
-// SPI MISO     12      Not used
-// SPI SCK      13      (Blue)
+#define pinEnableRelay A0
+#define pinRangeRelay A1
+
+#define pinRangeSwitch 5
+#define pinOutputEnableSwitch 6
+#define pinEncoderSwitch 7
+#define pinEncoderA 8
+#define pinEncoderB 9
 
 #define DISPLAY_SS 10
 #define DISPLAY_MOSI 11
-// 12
+//#define PIN_MISO     12      (not used for display)
 #define DISPLAY_SCK 13
-#define pinOutputEnableSwitch A0
 
-#define pinEncoderSwitch A1
-#define pinEncoderA A2
-#define pinEncoderB A3
+
 
 // i²c SCK      A4
 // i²c SCL      A5
 
 #define pinNTC A7
+
+//  Signal     Pin    Display wire
+// SPI SS       10      (Yellow)
+// SPI MOSI     11      (Purple)
+// SPI MISO     12      Not used
+// SPI SCK      13      (Blue)
 
 #endif
